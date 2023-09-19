1b9ad9200e3d946ade1fec153588e1b185dbd76a
rafa <askunknownsurname@gmail.com>
2023-09-19 09:18:04 -0300

    README stuff

---
+++ README.md
@@ -0,0 +1,33 @@
+# TODO
+PARSING CONF FILE
+- key values to some class
+Open a socket based on the CONF FILE
+Handle Req and Res accordingly to RFC.
+
+# TODO TONIGHT
+- REQ, RES, CONF Struct
+# TODO NEXT
+- Deal with GET and Path (how to deal with resources)
+- Stream data mechanism to deliver data to the client (Text, documents, video, etc).
+
+
+
+
+Client Request Header Example:
+GET / HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Cache-Control: max-age=0
+sec-ch-ua: "Microsoft Edge";v="117", "Not;A=Brand";v="8", "Chromium";v="117"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+DNT: 1
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate, br
+Accept-Language: pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
--- main.cpp
+++ main.cpp
@@ -53,43 +53,59 @@ void socket_listen(int server_socket) {
 void handle_request(int server_socket) {
 	while (true) {
 	// Accept incoming connections
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-        if (client_socket == -1) {
-            perror("Accepting connection failed");
-            continue; // Handle the error and continue accepting connections
-        }
+		struct sockaddr_in client_addr;
+		socklen_t client_addr_len = sizeof(client_addr);
+		int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+		if (client_socket == -1) {
+			perror("Accepting connection failed");
+			continue; // Handle the error and continue accepting connections
+		}
 
-        // Communication with the client
-        char buffer[2024];
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received == -1) {
-            perror("Receiving data failed");
-        } else {
-            // Handle received data
-            // ...
+		// Communication with the client
+		char buffer[1024];
+		int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+		if (bytes_received == -1) {
+			perror("Receiving data failed");
+		} else {
+			// Handle received data
+			// ...
+			std::string string_buffer(buffer);
+			// Send a response
+			size_t newlinePos = string_buffer.find('\n');
 
-            // Send a response
-			std::cout << buffer << "\n";
+			if (newlinePos != std::string::npos) {
+				// Extract the first line from the buffer.
+				std::string firstLine = string_buffer.substr(0, newlinePos);
+
+				// Print the first line.
+				std::cout << firstLine << std::endl;
+			} else {
+				// If no newline character is found, the entire buffer is considered as the first line.
+				std::cout << "Buffer contains only one line: " << string_buffer << std::endl;
+			}
+
+			// std::cout << buffer << "\r\n";
+
 			// Construct an HTTP response.
 			std::string httpResponse = "HTTP/1.1 200 OK\r\n";
 			httpResponse += "Content-Type: text/html\r\n"; // Set the content type
 			httpResponse += "Connection: close\r\n"; // Close the connection after sending the response
 			httpResponse += "\r\n"; // Empty line separates headers from the body
 			httpResponse += "<html><body><h1>Hello, World!</h1></body></html>";
-            int bytesSent = send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
+			int bytesSent = send(client_socket, httpResponse.c_str(), httpResponse.size(), 0);
 			if (bytesSent < 0) {
-        		std::cerr << "Error sending response to client" << std::endl;
-    		}
-        }
+				std::cerr << "Error sending response to client" << std::endl;
+			}
+		}
 		
-        // Close the client socket
-        close(client_socket);
+		// Close the client socket
+		close(client_socket);
 	}
 }
 
-int main() {
+int main(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
 	// Create a socket
 	int server_socket = socket_create();
 	// Set Options to the socket
@@ -100,7 +116,7 @@ int main() {
 	socket_listen(server_socket);
 	// Handle accept incoming requests
 	handle_request(server_socket);
-    // Close the server socket
-    close(server_socket);
-    return 0;
+	// Close the server socket
+	close(server_socket);
+	return 0;
 }
